a4d30750af4b81495881e2edeba57538741af5b9
minhthang165 <minhthangqhqn@gmail.com>
2025-07-15 19:25:15 +0700

    add cloudinary

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ using PRN_Final_Project.Repositories;
 using PRN_Final_Project.Repositories.Interface;
 using PRN_Final_Project.Service;
 using PRN_Final_Project.Service.Interface;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using dotenv.net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,8 +19,14 @@ builder.Services.AddDbContext<PRNDbContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
                      new MySqlServerVersion(new Version(8, 0, 2))));
 
+DotEnv.Load(options: new DotEnvOptions(probeForEnv: true));
+Cloudinary cloudinary = new Cloudinary(Environment.GetEnvironmentVariable("CLOUDINARY_URL"));
+cloudinary.Api.Secure = true;
+
 builder.Services.AddScoped<IClassService, ClassService>();
 builder.Services.AddScoped<IClassRepository, ClassRepository>();
+
+
 
 var app = builder.Build();
 
