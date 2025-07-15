3549ae472ef48efc374b26d46ea156a3c6faba38
AnhTuanFPT1303 <tuannha.ber133@gmail.com>
2025-07-15 20:09:25 +0700

    add repo

---
+++ Service/Interface/IClassService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PRN_Final_Project.Business.Entities;
+using PRN_Final_Project.Service.Common;
+
+namespace PRN_Final_Project.Service.Interface
+{
+    public interface IClassService : ICommonService<Class>
+    {
+        
+    }
+}
