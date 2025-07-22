7fee8d9e572d6879b0db3e69edb018b6912b27cc
AnhTuanFPT1303 <tuannha.ber133@gmail.com>
2025-07-22 11:36:12 +0700

    side css

--- wwwroot/js/sidebar.js
+++ wwwroot/js/sidebar.js
@@ -1,20 +1,19 @@
-﻿
-    document.addEventListener("DOMContentLoaded", function () {
-        const sidebarToggle = document.getElementById('sidebar-toggle');
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const sidebarToggle = document.getElementById('sidebar-toggle');
     const sidebar = document.getElementById('sidebar');
     const wrapper = document.querySelector('.app__slide-wrapper');
-        const subMenus = document.querySelectorAll('.sub-menu > a');
+    const subMenus = document.querySelectorAll('.sub-menu > a');
 
     sidebarToggle.addEventListener('click', function () {
         sidebar.classList.toggle('collapsed');
-    wrapper.classList.toggle('sidebar-collapsed');
-        });
+        wrapper.classList.toggle('sidebar-collapsed');
+    });
 
-        subMenus.forEach(menu => {
+    subMenus.forEach(menu => {
         menu.addEventListener('click', function (e) {
             e.preventDefault();
             const parent = this.parentElement;
             parent.classList.toggle('open');
-        });
-        });
+         });
     });
+});
